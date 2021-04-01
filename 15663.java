f687d634e4256bd33db9e955f8ceee9fea8168d4
Qulip <77991314+Qulip@users.noreply.github.com>
2021-04-02 02:35:19 +0900

    Change Code 15663

--- 15663.java
+++ 15663.java
@@ -6,6 +6,7 @@ import java.util.StringTokenizer;
 
 public class Main {
     static StringBuilder sb = new StringBuilder();
+    static boolean[] chk;
     static int[] rst;
     static int[] nums;
     public static void main(String[] args) throws IOException {
@@ -13,27 +14,36 @@ public class Main {
         StringTokenizer st = new StringTokenizer(input.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
+        chk = new boolean[n];
         rst = new int[m];
-        st = new StringTokenizer(input.readLine());
         nums = new int[n];
+        st = new StringTokenizer(input.readLine());
         for(int i=0; i<n; i++){
             nums[i]=Integer.parseInt(st.nextToken());
         }
         Arrays.sort(nums);
-        dfs(n,m,1,0);
-        System.out.print(sb);
+        dfs(n,m,0);
+        System.out.println(sb);
     }
-    static void dfs(int n, int m, int at, int time){
-        if(time==m){
+    static void dfs(int n, int m, int time){
+        if(time ==m){
             for(int i=0; i<rst.length; i++){
                 sb.append(rst[i]+" ");
             }
             sb.append("\n");
-        }else{
-            for(int i=at; i<=n; i++){
-                rst[time] = nums[i-1];
-                dfs(n,m, at, time+1);
-                at++;
+            return;
+        }
+        for(int i=0; i<n; i++){
+            if(!chk[i]){
+                if(i<n-1){
+                    if(nums[i]==nums[i+1]&&!chk[i+1]){
+                        continue;
+                    }
+                }
+                chk[i]=true;
+                rst[time] = nums[i];
+                dfs(n,m,time+1);
+                chk[i]=false;
             }
         }
     }
