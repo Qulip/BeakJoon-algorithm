138d7544aca5422c198042bec74d8c275890754c
Qulip <77991314+Qulip@users.noreply.github.com>
2021-04-04 18:36:26 +0900

    Modify 10989

--- 10989.java
+++ 10989.java
@@ -1,8 +1,29 @@
 package beak10989;
 
 import java.io.*;
-import java.util.Arrays;
 
+public class Main {     //no sorting
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(input.readLine());
+        StringBuilder sb = new StringBuilder();
+        int[] nums = new int[10000];
+        int tmp;
+        for(int i=0; i<tc; i++) {
+            tmp=Integer.parseInt(input.readLine());
+            nums[tmp-1]++;
+        }
+        for(int i=0; i<10000; i++){
+            if(nums[i]>0){
+                for(int j=0; j<nums[i]; j++){
+                    sb.append(i+1+"\n");
+                }
+            }
+        }
+        System.out.print(sb);
+    }
+}
+/* sorting
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
@@ -19,3 +40,5 @@ public class Main {
         System.out.print(sb);
     }
 }
+
+ */
