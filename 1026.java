f118043aafceb47f8c7509124870d51bc425d029
Qulip <ilkwon.ryu@gmail.com>
2021-04-10 21:18:20 +0900

    Solved 1026

---
+++ 1026.java
@@ -0,0 +1,29 @@
+package beak1026;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        int[] b = new int[n];
+        int rst = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for(int i=0; i<n; i++){
+            rst = rst + (a[n-i-1]*b[i]);
+        }
+        System.out.println(rst);
+    }
+}
