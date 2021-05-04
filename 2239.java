7557245cad15cbbf5f4ea6b2154f68c587527fd4
Qulip <ilkwon.ryu@gmail.com>
2021-05-04 21:03:30 +0900

    Solved 2239

--- 2239.java
+++ 2239.java
@@ -3,9 +3,11 @@ package beak2239;
 import java.io.*;
 
 public class Main {
+
+    static int[][] sudoku = new int[9][9];
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[][] sudoku = new int[9][9];
         for(int i=0; i<9; i++){
             int num = Integer.parseInt(br.readLine());
             for(int j=8; j>=0; j--){
@@ -13,30 +15,24 @@ public class Main {
                 num = num/10;
             }
         }
-        int[] now = next_zero(sudoku, 0, 0);
-        make_sudoku(sudoku, now[0], now[1]);
-
-        for(int i=0; i<9; i++){
-            for(int j=0; j<9; j++){
-                System.out.print(sudoku[i][j]);
-            }
-            System.out.println();
-        }
+        int[] now = next_zero(0, -1);
+        make_sudoku(now[0], now[1]);
+        print_sudoku();
     }
 
-    static boolean make_sudoku(int[][] sudoku, int now_col, int now_row){
+    static boolean make_sudoku(int now_row, int now_col){
         if(now_row==9){
             return true;
         }
-        int[] next = next_zero(sudoku, now_col, now_row);
-        boolean[] chk = new boolean[10];                        //t이 없는거 f있는거
-        chk_row(sudoku, now_col, chk);
-        chk_col(sudoku, now_row, chk);
-        chk_square(sudoku, now_row, now_col, chk);
+        int[] next = next_zero(now_row, now_col);
+        boolean[] chk = new boolean[10];                        //0이 없는거 1있는거
+        chk = chk_row(now_row, chk);
+        chk = chk_col(now_col, chk);
+        chk = chk_square(now_row, now_col, chk);
         for(int i=1; i<=9; i++) {
-            if(chk[i]){
+            if(!chk[i]){
                 sudoku[now_row][now_col] = i;
-                boolean next_sudoku = make_sudoku(sudoku, next[0], next[1]);
+                boolean next_sudoku = make_sudoku(next[0], next[1]);
                 if(next_sudoku){
                     return true;
                 }
@@ -45,52 +41,37 @@ public class Main {
         }
         return false;
     }
-    static boolean chk_row(int[][] sudoku, int now_col, boolean[] chk){
-        boolean[] nums = new boolean[10];
+    static boolean[] chk_row(int now_row, boolean[] chk){
         for(int i=0; i<9; i++){
-            chk[sudoku[now_col][i]] = false;
-            if(nums[sudoku[now_col][i]]) {
-                nums[sudoku[now_col][i]] = false;
-            }else{
-                return false;
-            }
+            chk[sudoku[now_row][i]] = true;
         }
-        return true;
+        return chk;
     }
 
-    static boolean chk_col(int[][] sudoku, int now_row, boolean[] chk){
-        boolean[] nums = new boolean[10];
+    static boolean[] chk_col(int now_col, boolean[] chk){
         for(int i=0; i<9; i++){
-            chk[sudoku[i][now_row]] = false;
-            if(nums[sudoku[i][now_row]]) {
-                nums[sudoku[i][now_row]] = false;
-            }else{
-                return false;
-            }
+            chk[sudoku[i][now_col]] = true;
         }
-        return true;
+        return chk;
     }
 
-    static boolean chk_square(int[][] sudoku, int now_row, int now_col, boolean[] chk){
-        boolean[] nums = new boolean[10];
-        for(int i=now_col; i<now_col+3; i++){
-            for(int j=now_row; j<now_row+3; j++) {
-                chk[sudoku[now_col][now_row]] = false;
-                if (nums[sudoku[now_col][now_row]]) {
-                    nums[sudoku[now_col][now_row]] = false;
-                } else {
-                    return false;
-                }
+    static boolean[] chk_square(int now_row, int now_col, boolean[] chk){
+        now_col = (now_col/3)*3;
+        now_row = (now_row/3)*3;
+        for(int i=now_row; i<now_row+3; i++){
+            for(int j=now_col; j<now_col+3; j++) {
+                chk[sudoku[i][j]] = true;
+                //System.out.print(sudoku[i][j]);
             }
         }
-        return true;
+        return chk;
     }
 
-    static int[] next_zero(int[][] sudoku, int now_row, int now_col){
+    static int[] next_zero(int now_row, int now_col){
         int[] next = new int[2];
         for(int i = now_row; i<9; i++){
             if(i==now_row) {
-                for (int j = now_col; j < 9; j++) {
+                for (int j = now_col+1; j < 9; j++) {
                     if (sudoku[i][j] == 0) {
                         next[0] = i;
                         next[1] = j;
@@ -110,6 +91,16 @@ public class Main {
         next[0] = 9;
         next[1] = 9;
         return next;
+    }
+    static void print_sudoku(){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                sb.append(sudoku[i][j]);
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
     }
 }
 /*
