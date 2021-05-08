f6eff87f628b2e8ff79e0d305c2ddb680bfaff83
Qulip <ilkwon.ryu@gmail.com>
2021-05-09 03:41:46 +0900

    Solved 11727

--- 11727.java
+++ 11727.java
@@ -4,8 +4,17 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int length = Integer.parseInt(bf.readLine());
 
+        //bottom-up
+        int d[] = new int[length+1];
+        d[0] = 1;
+        d[1] = 1;
+        for (int i = 2; i <= length; i++){
+            d[i] = d[i-1] + d[i-2] + d[i-2];
+            d[i] %= 10007;
+        }
+        System.out.println(d[length]);
     }
 }
