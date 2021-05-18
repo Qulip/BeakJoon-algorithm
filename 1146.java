d739090e17866dc28eefc29a9206eff694b7b731
Qulip <77991314+Qulip@users.noreply.github.com>
2021-05-18 23:25:15 +0900

    Solving 1146

---
+++ 1146.java
@@ -0,0 +1,16 @@
+package beak1146;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int distance = Integer.parseInt(st.nextToken());
+        for(int i=0; i<n; i++){
+
+        }
+    }
+}
