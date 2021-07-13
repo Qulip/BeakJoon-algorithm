06d306155ebdb2c37dd9b9b90555610d7cdc8c5d
Qulip <77991314+Qulip@users.noreply.github.com>
2021-07-13 23:31:48 +0900

    Add 9095

---
+++ 9095.java
@@ -0,0 +1,19 @@
+package beak9095;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for(int i=0; i<T; i++){
+            int n = Integer.parseInt(br.readLine());
+            int left_2 = n%2;
+            int rst_2 = n/2;
+            int left_3 = n%3;
+            int rst_3 = n/3;
+
+        }
+    }
+}
