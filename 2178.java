a6a510889f71e35750a1129b42345fa853b53cda
Qulip <77991314+Qulip@users.noreply.github.com>
2021-08-21 23:48:03 +0900

    Solving 2178

--- 2178.java
+++ 2178.java
@@ -4,7 +4,9 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
+    static int rst = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException{
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int rst = 1;
     }
 }
