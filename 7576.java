5e8b62c2ff34c058d5ad764f53ef8435c3fa62db
Qulip <77991314+Qulip@users.noreply.github.com>
2021-09-05 23:57:20 +0900

    Solving 7576

--- 7576.java
+++ 7576.java
@@ -9,22 +9,59 @@ public class Main {
     static int m;
     static int n;
     static int[][] map;
-    static boolean[][] visited;
+    static boolean[][] changed;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    static int all_tomato = 0;
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
         map = new int[n][m];
-        visited = new boolean[n][m];
         for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<m; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j]==1){
+                    all_tomato++;
+                }
             }
         }
-
+        int time = 1;
+        while(true){
+            changed = new boolean[n][m];
+            int before = all_tomato;
+            for(int i=0; i<n; i++){
+                for(int j=0; j<m; j++){
+                    if(map[i][j]==1&&!changed[i][j]){
+                        change_1(i,j);
+                    }
+                }
+            }
+            if(before==all_tomato){
+                time = -1;
+                break;
+            }
+            time++;
+        }
+        System.out.println(time);
     }
+    public static void change_1(int x, int y){
+        for(int i=0; i<4; i++){
+            int nextX = x + dr[i];
+            int nextY = y + dc[i];
+            if(nextX>=0 && nextY>=0 && nextX<n && nextY<n){
+                if(map[nextX][nextY]==0) {
+                    map[nextX][nextY] = 1;
+                    changed[nextX][nextY] = true;
+                    all_tomato++;
+                }
+            }
+        }
+    }
+    /*
     public void bfs(int x, int y){
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{x,y});
@@ -38,6 +75,7 @@ public class Main {
 
         }
     }
+     */
 }
 /*
 6 4
