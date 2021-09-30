e273728fa14ee4b4b233b9ae08a7fd258a2781b2
Qulip <77991314+Qulip@users.noreply.github.com>
2021-09-30 23:43:43 +0900

    Solving 1697

--- 1697.java
+++ 1697.java
@@ -13,10 +13,24 @@ public class Main {
         int x = Integer.parseInt(st.nextToken());
         int y = Integer.parseInt(st.nextToken());
         best = Integer.MAX_VALUE;
-        DFS(x,y);
+        DFS(x,y,0);
+        System.out.println(best);
     }
 
-    public static void DFS(int x, int y){
-
+    public static void DFS(int x, int y, int time){
+        int dist = Math.abs(y-x);
+        if(dist ==0){
+            best = Math.min(best, time);
+            return;
+        }
+        if(!(dist<Math.abs(y-x*2))){
+            DFS(x*2, y, time+1);
+        }
+        if(!(dist<Math.abs(y-x-1))) {
+            DFS(x + 1, y, time + 1);
+        }
+        if(!(dist<Math.abs(y-x+1))) {
+            DFS(x - 1, y, time + 1);
+        }
     }
 }
