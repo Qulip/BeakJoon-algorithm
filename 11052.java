c55f1195697318e2d8c34e370c0afd9b0d1464ec
Qulip <77991314+Qulip@users.noreply.github.com>
2021-10-16 23:58:44 +0900

    Solves 11052

--- 11052.java
+++ 11052.java
@@ -4,13 +4,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        int[] cardPack = new int[n+1];
+        int[] maxArr = new int[n+1];
+        cardPack[0] = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] cards = new int[n];
-        for(int i=0; i<n; i++){
-            cards[i] = Integer.parseInt(st.nextToken());
+
+        for(int i = 1 ; i <= n ; i++) {
+            cardPack[i] = Integer.parseInt(st.nextToken());
         }
+
+        for(int i = 1; i <=n; i++) {
+            for(int j = 1; j <=i; j++) {
+                maxArr[i] = Math.max(maxArr[i],cardPack[j]+maxArr[i-j]);
+            }
+        }
+        System.out.println(maxArr[n]);
     }
 }
